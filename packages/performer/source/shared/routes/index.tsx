700de01b01de82b2126a6f9d1e30ea912d2f67c0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-07-26 08:23:46 +0300

    performer > setup: package

---
+++ packages/performer/source/shared/routes/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import {
+    PluridRoute,
+} from '@plurid/plurid-data';
+
+import IndexPagePlane from '../kernel/planes/Index/Page';
+import NotFoundPlane from '../kernel/planes/NotFound';
+
+import Head from '../kernel/components/Head';
+
+
+
+const indexRoute: PluridRoute = {
+    value: '/',
+    exterior: {
+        kind: 'react',
+        element: Head,
+    },
+    spaces: [
+        {
+            value: 'default',
+            universes: [
+                {
+                    value: 'default',
+                    clusters: [
+                        {
+                            value: 'default',
+                            planes: [
+                                {
+                                    value: '/page',
+                                    component: {
+                                        kind: 'react',
+                                        element: IndexPagePlane,
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
+
+
+const notFoundRoute: PluridRoute = {
+    value: '/not-found',
+    exterior: {
+        kind: 'react',
+        element: () => (
+            <Head
+                title="not found · performer"
+            />
+        ),
+    },
+    spaces: [
+        {
+            value: 'default',
+            universes: [
+                {
+                    value: 'default',
+                    clusters: [
+                        {
+                            value: 'default',
+                            planes: [
+                                {
+                                    value: '/',
+                                    component: {
+                                        kind: 'react',
+                                        element: NotFoundPlane,
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
+
+const routes: PluridRoute[] = [
+    indexRoute,
+    notFoundRoute,
+];
+
+
+export default routes;
---
+++ packages/performer/source/shared/shell/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {
+    PluridComponent,
+} from '@plurid/plurid-react';
+
+import {
+    GlobalStyle,
+} from './styled';
+
+
+
+export interface ShellProperties {
+}
+
+const Shell: React.FC<ShellProperties> = (
+    properties,
+) => {
+    /** properties */
+    const {
+        children,
+    } = properties;
+
+
+    /** render */
+    return (
+        <>
+            <GlobalStyle />
+
+            {children}
+        </>
+    );
+}
+
+
+const shell: PluridComponent = {
+    kind: 'react',
+    element: Shell,
+};
+
+
+export default shell;
