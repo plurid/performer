878ad9459c912107374777087283df20dc607ddd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-07-26 08:47:19 +0300

    performer > dev: public

--- packages/performer/source/shared/kernel/components/Head/index.tsx
+++ packages/performer/source/shared/kernel/components/Head/index.tsx
@@ -50,11 +50,13 @@ const Head: React.FC<HeadProperties> = (
             <meta name="description" content={descriptionValue} />
 
             <link rel="icon" href="/favicon.ico" sizes="64x64" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="shortcut icon" type="image/png" href="/icon-192x192.png" />
             <link rel="shortcut icon" sizes="192x192" href="/icon-192x192.png" />
             <meta name="theme-color" content="#272A30" />
 
-            <link rel="manifest" href="/manifest.json" />
+            <link rel="manifest" href="/site.webmanifest" />
 
             {canonicalURL && (
                 <link rel="canonical" href={canonicalURL} />
@@ -64,7 +66,7 @@ const Head: React.FC<HeadProperties> = (
             <meta property="og:type" content="website" />
             <meta property="og:title" content={ogTitleValue} />
             <meta property="og:image" content={ogImageValue} />
-            <meta property="og:site_name" content="plurid" />
+            <meta property="og:site_name" content="performer" />
             <meta property="og:url" content={ogURLValue} />
             <meta property="og:description" content={ogDescriptionValue} />
 
@@ -79,11 +81,8 @@ const Head: React.FC<HeadProperties> = (
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-status-bar-style" content="default" />
             <meta name="apple-mobile-web-app-title" content={titleValue} />
-            <link rel="apple-touch-icon" href="/icon-192x192.png" />
-
-            {/* MICROSOFT */}
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#456c77" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#272A30" />
         </Helmet>
     );
 }
