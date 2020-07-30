2c151e0f153f514007a0d98a5c565124caf53c69
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-07-30 12:13:48 +0300

    performer > dev: shared/kernel > graphql

--- packages/performer/source/shared/kernel/services/graphql/mutate/index.ts
+++ packages/performer/source/shared/kernel/services/graphql/mutate/index.ts
@@ -16,10 +16,36 @@ export const SETUP_PROVIDER = gql`
     }
 `;
 
+export const OBLITERATE_PROVIDER = gql`
+    mutation ObliterateProvider($input: InputValueString!) {
+        obliterateProvider(input: $input) {
+            status
+            error {
+                type
+                path
+                message
+            }
+        }
+    }
+`;
+
 
 export const SETUP_WEBHOOK = gql`
     mutation SetupWebhook($input: InputSetupWebhook!) {
         setupWebhook(input: $input) {
+            status
+            error {
+                type
+                path
+                message
+            }
+        }
+    }
+`;
+
+export const OBLITERATE_WEBHOOK = gql`
+    mutation ObliterateWebhook($input: InputValueString!) {
+        obliterateWebhook(input: $input) {
             status
             error {
                 type
@@ -44,10 +70,36 @@ export const ADD_TRIGGER = gql`
     }
 `;
 
+export const OBLITERATE_TRIGGER = gql`
+    mutation ObliterateTrigger($input: InputValueString!) {
+        obliterateTrigger(input: $input) {
+            status
+            error {
+                type
+                path
+                message
+            }
+        }
+    }
+`;
+
 
 export const LINK_REPOSITORY = gql`
     mutation LinkRepository($input: InputLinkRepository!) {
         linkRepository(input: $input) {
+            status
+            error {
+                type
+                path
+                message
+            }
+        }
+    }
+`;
+
+export const UNLINK_REPOSITORY = gql`
+    mutation UnlinkRepository($input: InputValueString!) {
+        unlinkRepository(input: $input) {
             status
             error {
                 type
