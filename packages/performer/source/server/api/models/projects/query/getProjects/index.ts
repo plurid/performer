1c7cbd2609fd50e4d960ffa8cb8f98bdb24dc486
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-18 21:23:34 +0300

    performer > dev: server/api

--- packages/performer/source/server/api/models/projects/query/getProjects/index.ts
+++ packages/performer/source/server/api/models/projects/query/getProjects/index.ts
@@ -3,25 +3,126 @@
     import {
         Context,
     } from '#server/data/interfaces';
+
+    import {
+        generateMethodLogs,
+    } from '#server/utilities';
     // #endregion external
 // #endregion imports
 
 
 
 // #region module
+export const getProjectsLogs = generateMethodLogs('getProjects');
+
 const getProjects = async (
     context: Context,
 ) => {
+    // #region context unpack
     const {
         projects,
-    } = context;
+        request,
 
-    return {
-        status: true,
-        data: [
-            ...projects,
-        ],
-    };
+        privateUsage,
+        privateOwnerIdentonym,
+
+        customLogicUsage,
+
+        logger,
+        logLevels,
+    } = context;
+    // #endregion context unpack
+
+
+    // #region log start
+    logger.log(
+        getProjectsLogs.infoStart,
+        logLevels.info,
+    );
+    // #endregion log start
+
+
+    try {
+        // #region private usage
+        if (privateUsage) {
+            logger.log(
+                getProjectsLogs.infoHandlePrivateUsage,
+                logLevels.trace,
+            );
+
+            if (!privateOwnerIdentonym) {
+                logger.log(
+                    getProjectsLogs.infoEndPrivateUsage,
+                    logLevels.info,
+                );
+
+                return {
+                    status: false,
+                };
+            }
+
+            logger.log(
+                getProjectsLogs.infoSuccessPrivateUsage,
+                logLevels.info,
+            );
+
+            return {
+                status: true,
+                data: [
+                    ...projects,
+                ],
+            };
+        }
+        // #endregion private usage
+
+
+        // #region logic usage
+        const logic = request.performerLogic;
+
+        if (customLogicUsage && logic) {
+            logger.log(
+                getProjectsLogs.infoHandleCustomLogicUsage,
+                logLevels.trace,
+            );
+
+            const owner = await logic.getCurrentOwner();
+
+            return {
+                status: true,
+                data: [
+                    ...owner.projects,
+                ],
+            };
+        }
+        // #endregion logic usage
+
+
+        // #region public usage
+        logger.log(
+            getProjectsLogs.infoSuccessCustomLogicUsage,
+            logLevels.info,
+        );
+
+        return {
+            status: true,
+            data: [
+                ...projects,
+            ],
+        };
+        // #endregion public usage
+    } catch (error) {
+        // #region error handle
+        logger.log(
+            getProjectsLogs.errorEnd,
+            logLevels.error,
+            error,
+        );
+
+        return {
+            status: false,
+        };
+        // #endregion error handle
+    }
 }
 // #endregion module
 
