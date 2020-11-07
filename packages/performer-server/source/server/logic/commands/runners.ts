e23b1738e90411898a38d67bfc76cd520a3dd21b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-07 18:03:21 +0200

    performer-server > dev: server/logic/commands

--- packages/performer-server/source/server/logic/commands/runners.ts
+++ packages/performer-server/source/server/logic/commands/runners.ts
@@ -336,7 +336,7 @@ export const runInContainer = (
         );
 
         readableStream.on('error', (error) => {
-            console.log('[Performr] :: runInContainer error', error);
+            console.log('[Performer] :: runInContainer error', error);
         });
 
         readableStream.on('end', async () => {
@@ -368,5 +368,40 @@ export const runInContainer = (
             resolve();
         });
     });
+}
+
+
+export const cleanDockerImages = async () => {
+    try {
+        const images = await docker.listImages();
+
+        for (const image of images) {
+            try {
+                const {
+                    Id: id,
+                    Created: created,
+                } = image;
+
+                const windowHours = 1;
+                const now = Math.floor(Date.now() / 1000);
+                // window hours ago
+                const past = now - 3600 * windowHours;
+                // window hours from now
+                const future = now + 3600 * windowHours;
+
+                if (created > past && created < future) {
+                    const dockerImage = docker.getImage(id);
+
+                    await dockerImage.remove();
+                }
+            } catch (error) {
+                console.log('Performer :: error cleanDockerImages > remove image', image.Id, error);
+                continue;
+            }
+        }
+    } catch (error) {
+        console.log('Performer :: error cleanDockerImages', error);
+        return;
+    }
 }
 // #endregion module
